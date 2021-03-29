d4b70858e6f0089d7c270ef248e3bb21e2d68214
Alexey Yakovtsov <hannisolo@yandex.ru>
2021-03-29 16:02:10 +0300

    task_3_1

--- homework_3/task_3_1.py
+++ homework_3/task_3_1.py
@@ -8,11 +8,16 @@ try:
     dd = int(input('Введите число d: '))
 
     x = PrettyTable()
-    x.field_names = ['Activity', 'Result']
+    x.field_names = ['Activity', 'Result', 'Activity2', 'Result2', 
+                    'Activity3', 'Result3', 'Activity4', 'Result4']
 
     if aa > 0 and bb > 0 and cc > 0 and dd > 0:
         for i in range(11):
-            x.add_row([f'{aa} * {i} =', aa * i])
+            x.add_row([f'{aa} * {i} =', aa * i, f'{bb} * {i} =', bb * i, 
+                        f'{cc} * {i} =', cc * i, f'{dd} * {i} =', dd * i])
+            #x.add_row([f'{bb} * {i} =', bb * i])
+            #x.add_row([f'{cc} * {i} =', cc * i])
+            #x.add_row([f'{dd} * {i} =', dd * i])
         print(x)
     else:
         print('Введеное значение отрицательно')
