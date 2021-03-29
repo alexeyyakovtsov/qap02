ba02209b9cd0725ab840b5b23301847c0a46bffe
Alexey Yakovtsov <hannisolo@yandex.ru>
2021-03-29 15:38:21 +0300

    refactoring task_3_1

--- homework_3/task_3_1.py
+++ homework_3/task_3_1.py
@@ -1,12 +1,21 @@
 from prettytable import PrettyTable
 
+
 try:
-    number = int(input('Введите цифру из таблицы умножения: '))
+    aa = int(input('Введите число a: '))
+    bb = int(input('Введите число b: '))
+    cc = int(input('Введите число c: '))
+    dd = int(input('Введите число d: '))
+
     x = PrettyTable()
     x.field_names = ['Activity', 'Result']
-    for i in range(11):
-        x.add_row([f'{number} * {i} =', number * i])
-    print(x)
+
+    if aa > 0 and bb > 0 and cc > 0 and dd > 0:
+        for i in range(11):
+            x.add_row([f'{aa} * {i} =', aa * i])
+            print(x)
+    else:
+        print('Введеное значение отрицательно')
 
 except ValueError:
     print('Ошибка - введено не число')
