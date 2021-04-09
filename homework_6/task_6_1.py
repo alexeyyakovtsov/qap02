f768f66f4b261bd8c74ed2c56c952164c491962f
Alexey Yakovtsov <hannisolo@yandex.ru>
2021-04-09 15:45:18 +0300

    add new homework_6

---
+++ homework_6/task_6_1.py
@@ -0,0 +1,39 @@
+class WinDoor:
+    def __init__(self, x, y, name="unk"):
+       self.x = x
+       self.y = y
+       self.name = name
+
+    def square(self):
+       self.square = self.x * self.y 
+
+    def __repr__(self):
+       return f'{self.name} {self.x}x{self.y}'
+
+class Room(WinDoor):
+    def __init__(self,x, y, z):
+        WinDoor.__init__(self, x, y, z)
+        self.z = z
+        self.wd = []
+
+    def square(self):
+        self.square = 2 * self.z * (self.x + self.y)
+        print(f'Полная площадь = {self.square}')
+
+    def addWD(self, w, h, name='unk'):
+        self.wd.append(WinDoor(w, h, name))
+        
+    def workSurface(self):
+        new_square = self.square()
+        for i in self.wd:
+            new_square -= i.square
+        print(f'Оклеиваемая площадь = {new_square}')
+        return new_square
+
+r1 = Room(1, 2, 3)
+r1.square()
+r1.addWD(1, 1) 
+r1.addWD(1, 1)
+r1.addWD(1, 2)
+print(r1.wd)
+r1.workSurface()
