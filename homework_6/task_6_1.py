43163f2910d71b64c39ad1f4aa36dee11df6a11a
Alexey Yakovtsov <hannisolo@yandex.ru>
2021-04-09 19:33:18 +0300

    refactoring homework_6

--- homework_6/task_6_1.py
+++ homework_6/task_6_1.py
@@ -1,41 +1,34 @@
 class WinDoor:
-    def __init__(self, x, y, name="unk"):
-       self.x = x
-       self.y = y
-       self.name = name
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
     def square(self):
-       self.square = self.x * self.y 
+        self.square = self.x * self.y
+        return self.square
 
-    #getter
-    def __repr__(self):
-       return f'{self.name} {self.x}x{self.y}'
-
-class Room(WinDoor):
-    def __init__(self,x, y, z):
-        WinDoor.__init__(self, x, y, z)
+class Room:
+    def __init__(self, x, y, z):
+        WinDoor.__init__(self, x, y)
         self.z = z
         self.wd = []
 
     def square(self):
         self.square = 2 * self.z * (self.x + self.y)
-        print(f'Полная площадь = {self.square}')
+        return self.square
 
-    #setter
-    def addWD(self, w, h, name='unk'):
-        self.wd.append(WinDoor(w, h, name))
-        
+    def addWD(self, w, h):
+        self.wd.append(WinDoor(w, h))
+
     def workSurface(self):
-        new_square = self.square
+        new_square = self.square()
         for i in self.wd:
-            new_square -= i.square
-        print(f'Оклеиваемая площадь = {new_square}')
+            new_square -= WinDoor.square(i)
         return new_square
+ 
 
-r1 = Room(int(input('Введите ширину: ')), int(input('Введите длинну: ')), int(input('Введите высоту: ')))
-r1.square()
+r1 = Room(float(input('Введите ширину: ')), float(input('Введите длинну: ')), float(input('Введите высоту: ')))
 r1.addWD(1, 1) 
 r1.addWD(1, 1)
 r1.addWD(1, 2)
-print(r1.wd)
-r1.workSurface()
+print(f'Общая площадьв: {r1.workSurface()}')
